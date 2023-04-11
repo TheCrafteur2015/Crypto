872260c196367f086b7628c57cfcd031fae3182c
Gabriel Roche <gabirocher@icloud.com>
2023-04-11 10:37:31 +0200

    Branche master

---
+++ Crypto/src/com/github/atlonos/crypto/main/TestCrypto.java
@@ -0,0 +1,22 @@
+package com.github.atlonos.crypto.main;
+
+/**
+ * @author Gabri
+ *
+ */
+public class TestCrypto {
+	
+	public TestCrypto() {
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
